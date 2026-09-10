08370b1ad327add10a7ced3073e7943144c598df
Ana Julia <anajcaetanom@gmail.com>
2026-09-10 14:47:14 -0300

    feat: nova fila na classe Device para comunicação com o ar

--- src/Device.py
+++ src/Device.py
@@ -5,20 +5,20 @@
 #                                                   #
 #####################################################
 
-import queue
 
-class Device():
+class Device:
 	ID = None           # ID do dispositivo
 	type = None         # ID do tipo
 	typeName = None     #     : descrição do tipo
-	typeCode = None     #     : simbolo T=Temperatura, L=Lâmpada, S=Sensor de presença
+	typeCode = None     #     : simbolo T=Temperatura, L=Lâmpada, S=Sensor de presença, A=Ar condicionado
 	roomID = None       # ID do ambiente
 	roomName = None     #     : nome do ambiente
 	status = None       # Status da última operação realizada
 	connection = None   # Conexão TCP
 	clientIP = None     # Endereço IP do dispositivo
 	value = None        # Último valor lido
-	lampQueue = None    # Fila para comunicação com o controle
+	lampQueue = None 	# Fila para comunicação com o controle
+	airQueue = None     # Fila para comunicação com o controle
 	buffer = ''
 
 	def __init__(self, connection, clientIP, controlQueue):
